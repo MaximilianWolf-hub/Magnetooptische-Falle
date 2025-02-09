d592f1b887af9803975fddbfeecea62a208bfd46
Maxi <maximilian.wolf@stud.tu-darmstadt.de>
2025-02-10 00:02:17 +0100

    erste Bearbeitungen zur Temperaturbestimmung in der MOT

---
+++ MOT_Messung 2.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+data = [203, 211.5, 196.3, 194.9, 247, 258, 229, 246.6]
+
+print(np.mean(data))
+print(np.std(data))
--- Temperaturbestimmung_MOT_neu.py
+++ Temperaturbestimmung_MOT_neu.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os
+from uncertainties import ufloat
 import matplotlib.pyplot as plt
 import tifffile as tiff
 import glob
@@ -7,10 +7,17 @@ from Funktionen import background_average, fit_gaussian_2D
 import re
 from scipy.optimize import curve_fit
 
-messung_1 = glob.glob(r"C:\Users\Maximilian Wolf\Desktop\Uni\FP\Magnetooptische Falle\Wolf_Hesse\Ersatzdaten\240601\Temperaturbestimmung MOT\images_0000\Images\*.tiff")
+
+
+messung_1 = glob.glob(r"C:\Users\Maximilian Wolf\Desktop\Uni\FP\Magnetooptische Falle\Wolf_Hesse\Ersatzdaten\240601\Temperaturbestimmung Melasse\images_0003\Images\*.tiff")
 messung_1.sort(key=lambda f: float(re.search(r"tof_(\d+\.\d+)ms", f).group(1)))
 
-hintergrund_1 = glob.glob(r"C:\Users\Maximilian Wolf\Desktop\Uni\FP\Magnetooptische Falle\Wolf_Hesse\Ersatzdaten\240601\Temperaturbestimmung MOT\images_0000\Background\*.tiff")
+
+
+
+
+
+hintergrund_1 = glob.glob(r"C:\Users\Maximilian Wolf\Desktop\Uni\FP\Magnetooptische Falle\Wolf_Hesse\Ersatzdaten\240601\Temperaturbestimmung Melasse\images_0003\Background\*.tiff")
 
 average_hint = background_average(hintergrund_1)
 
@@ -53,10 +60,22 @@ p0_y = [max(Verlauf_sigma_y_square) - min(Verlauf_sigma_y_square) / max(time_squ
 params_x, cov_x = curve_fit(linear_fit, (time_square), Verlauf_sigma_x_square, p0_x)
 params_y, cov_y = curve_fit(linear_fit, (time_square), Verlauf_sigma_y_square, p0_y)
 
+#Unsicherheiten aus der Kovarianzmatrix berechnen
+perr_x = np.sqrt(np.diag(cov_x))
+perr_y = np.sqrt(np.diag(cov_y))
+
+Steigung_x = ufloat(params_x[0], perr_x[0])
+Steigung_y = ufloat(params_y[0], perr_y[0])
+
 lin_fit_x = linear_fit(time_square, params_x[0], params_x[1])
 lin_fit_y = linear_fit(time_square, params_y[0], params_y[1])
 
-print(params_x, params_y)
+print('Steigung sigma_x: ', Steigung_x, 'Steigung sigma_y: ', Steigung_y)
+
+Temperatur_x = 1e6 * Steigung_x * 84.912 * 1.66e-27 / 1.38e-23
+Temperatur_y = 1e6 * Steigung_y * 84.912 * 1.66e-27 / 1.38e-23
+
+print('Temperatur_x: ', Temperatur_x, 'Temperatur_y: ', Temperatur_y)
 
 #Plotten der Daten
 plt.plot(time_square, Verlauf_sigma_x_square)
@@ -64,7 +83,8 @@ plt.plot(time_square, Verlauf_sigma_y_square)
 plt.plot(time_square, lin_fit_x)
 plt.plot(time_square, lin_fit_y)
 plt.xlabel('$\mathrm{T}^2$ in $\mathrm{ms}^2$')
-plt.ylabel(r"$\sigma_x^2$ (blau) und $\sigma_y^2$ (orange) in $\mathrm{\mu m}^2$")
+plt.ylabel(r"$\sigma_x^2$ und $\sigma_y^2$ in $\mathrm{m}^2$")
+plt.savefig('Messung_1_Temperatur_MOT.png')
 plt.show()
 
 
